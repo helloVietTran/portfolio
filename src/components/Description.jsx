950bffe8265c9877d211bd6ff383eabf6db2ad26
helloVietTran <trandanhviet192003@gmail.com>
2025-06-20 14:59:55 +0700

    feature: dowload cv

--- src/components/Description.jsx
+++ src/components/Description.jsx
@@ -2,9 +2,7 @@ import { motion } from 'framer-motion';
 
 export default function Description() {
   return (
-    <div
-      className="grid items-center gap-8 md:grid-cols-2"
-    >
+    <div className="grid items-center gap-8 md:grid-cols-2">
       <div className="relative isolate mx-auto max-w-[400px]">
         <img
           src="assets/bg.png"
@@ -35,19 +33,23 @@ export default function Description() {
         </h2>
         <p className="mb-8 text-gray-400 md:pr-4 lg:pr-12">
           Em là một sinh viên yêu thích công nghệ và đang theo đuổi con đường
-          trở thành lập trình viên Fullstack. Với tinh thần trách nhiệm và
-          sự kiên trì trong công việc, em luôn nỗ lực hoàn thiện từng công việc
-          và không ngừng học hỏi để nâng cao kỹ năng của mình.
+          trở thành lập trình viên Fullstack. Với tinh thần trách nhiệm và sự
+          kiên trì trong công việc, em luôn nỗ lực hoàn thiện từng công việc và
+          không ngừng học hỏi để nâng cao kỹ năng của mình.
           <br /> Em đặc biệt yêu thích việc xây dựng phần mềm một cách chuyên
           nghiệp – từ việc tối ưu hoá trải nghiệm người dùng ở frontend đến việc
-          thiết kế backend hiệu quả và dễ bảo trì. Trong giai đoạn học
-          tập, em đã thử áp dụng quy trình phát triển phần học được ở
-          trường đại học vào sản phẩm của mình.
+          thiết kế backend hiệu quả và dễ bảo trì. Trong giai đoạn học tập, em
+          đã thử áp dụng quy trình phát triển phần học được ở trường đại học vào
+          sản phẩm của mình.
         </p>
         <div>
-          <button className="cursor-pointer rounded-full bg-gradient-to-b from-[#a78bfa] to-[#7c3aed] px-6 py-3 text-base font-medium text-white shadow-[5px_5px_10px_rgba(100,100,100,0.2)]">
+          <a
+            href="/CV_TranVietAnh_vn.pdf"
+            download
+            className="rounded-full bg-gradient-to-b from-[#a78bfa] to-[#7c3aed] px-6 py-3 text-base font-medium text-white shadow-[5px_5px_10px_rgba(100,100,100,0.2)]"
+          >
             Tải CV
-          </button>
+          </a>
         </div>
       </motion.div>
     </div>
